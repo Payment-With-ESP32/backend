28adaf944880b653a8de6084a942eb4ca385c338
PromotezCitizen <gkstkdgus821@gmail.com>
2025-07-28 21:30:47 +0900

    mac 주소를 받을 때 trim, lowercase 적용 및 prelong 되지 않던 현싱 수정

--- src/main/kotlin/com/example/payments/config/ESP32ConnectionPoolCache.kt
+++ src/main/kotlin/com/example/payments/config/ESP32ConnectionPoolCache.kt
@@ -11,6 +11,7 @@ import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.io.File
 import java.nio.charset.StandardCharsets
+import kotlin.collections.HashSet
 import kotlin.concurrent.thread
 
 @Component
@@ -30,9 +31,9 @@ class ESP32ConnectionPoolCache(
         if (file.exists()) {
             val jsonText = file.readText(charset = StandardCharsets.UTF_8)
             val data: ESP32Data = objectMapper.readValue(jsonText, ESP32Data::class.java)
-            masterMac = data.masterMac.trim()
-            data.slaveMacs.forEach { it.macAddress = it.macAddress.trim() }
-            slaveMacs = data.slaveMacs.filter { it.macAddress != masterMac }.toMutableSet()
+            masterMac = data.masterMac.trim().lowercase()
+            data.slaveMacs.forEach { it.macAddress = it.macAddress.trim().lowercase() }
+            slaveMacs = data.slaveMacs.toMutableSet()
         }
 
         thread(start = true) {
@@ -43,7 +44,7 @@ class ESP32ConnectionPoolCache(
                     if (commands.size == 2) {
                         when (commands[0]) {
                             "MAC" -> {
-                                if (masterMac != commands[1]) masterMac = commands[1]
+                                if (masterMac != commands[1]) masterMac = commands[1].trim().lowercase()
                             }
                             else -> {}
                         }
@@ -69,43 +70,52 @@ class ESP32ConnectionPoolCache(
     fun getSlaves(): List<ESP32Position> = slaveMacs.toList()
     fun getMaster(): String = masterMac
     fun upsertSlave(macAddress: String, position: ESP32PositionDetail) {
-        val module = slaveMacs.find { it.macAddress == macAddress }
+        val loweredMacAddress = macAddress.trim().lowercase()
+
+        val module = slaveMacs.find { it.macAddress == loweredMacAddress }
         if (module != null) {
             module.position = position
         } else {
-            slaveMacs.add(ESP32Position(macAddress, position))
-            val addCommand = "[ADD]$macAddress"
+            slaveMacs.add(ESP32Position(loweredMacAddress, position))
+            val addCommand = "[ADD]$loweredMacAddress"
             serialCommManager.send(addCommand)
         }
     }
     fun changeModule(prevMacAddress: String, upcomingMacAddress: String) {
-        val prevModule = slaveMacs.find { it.macAddress == prevMacAddress }
+        val loweredPrevMacAddress = prevMacAddress.trim().lowercase()
+        val loweredUpcomingMacAddress = upcomingMacAddress.trim().lowercase()
+
+        val prevModule = slaveMacs.find { it.macAddress == loweredPrevMacAddress }
         if (prevModule != null) {
             deleteSlave(prevModule.macAddress)
-            upsertSlave(upcomingMacAddress, prevModule.position)
-            val swapCommand = "[SWAP]$prevMacAddress/$upcomingMacAddress"
+            upsertSlave(loweredUpcomingMacAddress, prevModule.position)
+            val swapCommand = "[SWAP]$loweredPrevMacAddress/$loweredUpcomingMacAddress"
             serialCommManager.send(swapCommand)
         }
     }
     fun upsertMaster(macAddress: String) {
-        masterMac = macAddress
+        masterMac = macAddress.trim().lowercase()
+
         val connectCommand = "[INIT]${slaveMacs.joinToString(",") { it.macAddress }}"
         serialCommManager.send(connectCommand)
     }
     fun deleteSlave(macAddress: String) {
-        val dummy = ESP32Position(macAddress.trim(), ESP32PositionDetail())
+        val loweredMacAddress = macAddress.trim().lowercase()
+
+        val dummy = ESP32Position(loweredMacAddress, ESP32PositionDetail())
         slaveMacs.remove(dummy)
         val delCommand = "[DEL]$macAddress"
         serialCommManager.send(delCommand)
     }
     fun sendPreLongServiceTime(macAddress: String, time: Int) {
-        val preLongCommand = "[PREL]" + when (time) {
+        val loweredMacAddress = macAddress.trim().lowercase()
+
+        val preLongCommand = "[PREL]$loweredMacAddress/" + when (time) {
             -1 -> "OFF"
             0 -> "ON/0"
             else -> "ON/$time"
         }
         serialCommManager.send(preLongCommand)
-        println(preLongCommand)
     }
 }
 
